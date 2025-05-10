e48274af3cbb123f34b7cb84b5d77963889039b5
ryo-endo-develop <153417105+ryo-endo-develop@users.noreply.github.com>
2025-05-10 05:35:06 +0000

    User登録のテストコードの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
@@ -18,8 +19,8 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
-    end
+  end
 end
